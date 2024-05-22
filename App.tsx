4e4ca0d968dfb6e4671a1d9e15204e52ff117af4
Bhojraj Singh Shekhawat <bs31shekhawat@gmail.com>
2024-05-22 11:03:40 +0530

    Task_complete

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Home';
-import Signup from './src/Signup';
-import Login from './src/Login';
 import { LogBox, SafeAreaView, StatusBar } from 'react-native';
 import BottomNav from './app/navigation/BottomNav';
 
@@ -15,11 +12,6 @@ function App() {
   }, [])
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator> */}
       <SafeAreaView style={{ flex: 1, backgroundColor: '#123c6e' }}>
         <StatusBar backgroundColor="#123c6e" barStyle={'light-content'} />
         <BottomNav />
